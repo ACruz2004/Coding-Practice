c3efe937a07a1e6d6389b09aef576079793c2947
ACruz2004 <166861679+ACruz2004@users.noreply.github.com>
2024-04-15 13:28:33 -0400

    Add files via upload

---
+++ Lab3_Figures/Test.cpp
@@ -0,0 +1,22 @@
+//
+//  Test.cpp
+//  Lab3_Figures
+//
+//  Created by Anthony Cruz on 9/11/23.
+//
+
+#include <iostream>
+
+using std :: cin; using std :: cout; using std :: endl;
+
+int main() {
+    for (int i = 0; i < 4; ++i) {
+
+        if (i == 3)
+
+               break;
+
+        cout << i << " ";
+
+    }
+}
