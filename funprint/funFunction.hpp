13d255c7e639d3ded6dc7036748a2f401377f5e8
Anthony Cruz <166861679+ACruz2004@users.noreply.github.com>
2024-05-31 11:25:46 -0400

    Add files via upload

---
+++ funprint/funFunction.hpp
@@ -0,0 +1,74 @@
+//HPP File For funprint Functions
+//Was gonna do a class but it's obsolite fr fr
+#ifndef FUNFUNCTION_HPP
+#define FUNFUNCTION_HPP
+
+#include<iostream>
+#include<curses.h> 
+#include<cstring>
+#include<unistd.h>
+#include "textprint.hpp"
+
+//Function Declarations
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+void makeSpeed(); //Selection of speed
+void makeShape(); //Selection of shape
+
+//Code For The Functions
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+void makeSpeed() {
+
+    speed = speedInput;
+    if (!speedInput) {
+        std::cout << "Invalid, shutting down loser >:( " << std::endl;
+    }
+}
+
+void makeShape() {
+
+    if (shapeInput == "Square" || shapeInput == "square") {
+    for (int i = 0; i < 7; ++i) {
+        std::cout << std::endl;
+        for (int j = 0; j < 15; ++j) {
+            std::cout << "Ñ";
+            usleep(speed);
+            fflush( stdout );
+        }
+    }
+    std::cout << std::endl;
+    }
+
+
+if (shapeInput == "Amogus" || shapeInput == "amogus") {
+    std::string mate = "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣤⣤⣤⣀⣀⣀⣀⡀⠀⠀⠀⠀⠀⠀⠀ \n"
+"⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣼⠟⠉⠉⠉⠉⠉⠉⠉⠙⠻⢶⣄⠀⠀⠀⠀⠀ \n"
+"⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⣷⡀⠀⠀⠀ \n"
+"⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⡟⠀⣠⣶⠛⠛⠛⠛⠛⠛⠳⣦⡀⠀⠘⣿⡄⠀⠀ \n"
+"⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⠁⠀⢹⣿⣦⣀⣀⣀⣀⣀⣠⣼⡇⠀⠀⠸⣷⠀⠀ \n"
+"⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⡏⠀⠀⠀⠉⠛⠿⠿⠿⠿⠛⠋⠁⠀⠀⠀⠀⣿⡄⣠ \n"
+"⠀⠀⢀⣀⣀⣀⠀⠀⢠⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⡇⠀ \n"
+"⠿⠿⠟⠛⠛⠉⠀⠀⣸⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡇⠀ \n"
+"⠀⠀⠀⠀⠀⠀⠀⠀⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣧⠀ \n"
+"⠀⠀⠀⠀⠀⠀⠀⢸⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣿⠀ \n"
+"⠀⠀⠀⠀⠀⠀⠀⣾⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⠀ \n"
+"⠀⠀⠀⠀⠀⠀⠀⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⠀ \n"
+"⠀⠀⠀⠀⠀⠀⢰⣿⠀⠀⠀⠀⣠⡶⠶⠿⠿⠿⠿⢷⣦⠀⠀⠀⠀⠀⠀⠀⣿⠀ \n"
+"⠀⠀⣀⣀⣀⠀⣸⡇⠀⠀⠀⠀⣿⡀⠀⠀⠀⠀⠀⠀⣿⡇⠀⠀⠀⠀⠀⠀⣿⠀ \n"
+"⣠⡿⠛⠛⠛⠛⠻⠀⠀⠀⠀⠀⢸⣇⠀⠀⠀⠀⠀⠀⣿⠇⠀⠀⠀⠀⠀⠀⣿⠀ \n"
+"⢻⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣼⡟⠀⠀⢀⣤⣤⣴⣿⠀⠀⠀⠀⠀⠀⠀⣿⠀ \n"
+"⠈⠙⢷⣶⣦⣤⣤⣤⣴⣶⣾⠿⠛⠁⢀⣶⡟⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡟⠀ \n"
+"⢷⣶⣤⣀⠉⠉⠉⠉⠉⠄⠀⠀⠀⠀⠈⣿⣆⡀⠀⠀⠀⠀⠀⠀⢀⣠⣴⡾⠃⠀ \n"
+"⠀⠈⠉⠛⠿⣶⣦⣄⣀⠀⠀⠀⠀⠀⠀⠈⠛⠻⢿⣿⣾⣿⡿⠿⠟⠋⠁⠀⠀⠀ \n";
+    for (int j = 0; j < 1800; ++j) {
+        std::cout << mate[j];
+        usleep(speed);
+        fflush( stdout );
+        }
+    }
+    std::cout << std::endl;
+}
+    
+
+
+
+#endif
---
+++ funprint/funprint.cpp
@@ -0,0 +1,19 @@
+// Funprint 
+// Just found out how to sleep the command line so I'm gonna do some stuff with that 
+// W
+
+#include <iostream>
+#include "textprint.hpp"
+#include "funFunction.hpp"
+
+int main() {
+    //Greetings
+    greetings();
+    //Selection
+    shapeSelection();
+    speedSelection();
+    //Makes them
+    makeSpeed();
+    makeShape();
+    
+}
